dbc09d31dbae2c2c93e3154e206e6ec3cd5d1d78
Renan Valladão <renancvalladao@gmail.com>
2023-12-23 21:27:27 -0300

    Adding ground collision

--- Tappy/plane_cb/plane_cb.gd
+++ Tappy/plane_cb/plane_cb.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 const GRAVITY: float = 1900.0
 const POWER: float = -400.0
@@ -14,8 +15,16 @@ func _physics_process(delta):
 	fly()
 	
 	move_and_slide()
+	
+	if is_on_floor():
+		die()
 
 func fly() -> void:
 	if Input.is_action_just_pressed("fly"):
 			velocity.y = POWER
 			animation_player.play("fly")
+
+func die() -> void:
+	animated_sprite_2d.stop()
+	set_physics_process(false)
+	
