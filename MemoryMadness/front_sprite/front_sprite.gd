5d4cb58953e9574e025488f8944b8e397c808ea7
Renan Valladão <renancvalladao@gmail.com>
2023-12-27 14:59:04 -0300

    Creating FrontSprite scene

---
+++ MemoryMadness/front_sprite/front_sprite.gd
@@ -0,0 +1,14 @@
+extends TextureRect
+
+const SCALE_SMALL: Vector2 = Vector2(0.1, 0.1)
+const SCALE_NORMAL: Vector2 = Vector2(1.0, 1.0)
+const SCALE_TIME: float = 0.5
+
+func _ready():
+	run_me()
+
+func run_me() -> void:
+	var tween = get_tree().create_tween()
+	tween.set_loops()
+	tween.tween_property(self, "scale", SCALE_NORMAL, SCALE_TIME)
+	tween.tween_property(self, "scale", SCALE_SMALL, SCALE_TIME)
