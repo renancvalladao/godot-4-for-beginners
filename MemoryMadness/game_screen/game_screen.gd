6559d49d0b4d623f73e91c9935ad51affecf4be7
Renan Valladão <renancvalladao@gmail.com>
2023-12-29 11:06:47 -0300

    Selecting images on level selection

--- MemoryMadness/game_screen/game_screen.gd
+++ MemoryMadness/game_screen/game_screen.gd
@@ -3,9 +3,15 @@ extends Control
 @onready var sound = $Sound
 
 func _ready():
-	pass
+	SignalManager.on_level_selected.connect(on_level_selected)
 
 func _process(delta):
+	pass
+
+func on_level_selected(level_num: int) -> void:
+	var level_selection = GameManager.get_level_selection(level_num)
+	var frame_image = ImageManager.get_random_frame_image()
+	
 	pass
 
 func _on_exit_button_pressed():
--- MemoryMadness/singletons/image_manager.gd
+++ MemoryMadness/singletons/image_manager.gd
@@ -1,6 +1,12 @@
 extends Node
 
-var _items_images: Array = []
+const FRAME_IMAGES: Array = [
+	preload("res://assets/frames/blue_frame.png"),
+	preload("res://assets/frames/red_frame.png"),
+	preload("res://assets/frames/yellow_frame.png")
+]
+
+var _item_images: Array = []
 
 func _ready():
 	load_item_images()
@@ -13,7 +19,7 @@ func add_file_to_list(fn: String, path: String) -> void:
 		"item_texture": load(full_path)
 	}
 	
-	_items_images.append(ii_dict)
+	_item_images.append(ii_dict)
 
 func load_item_images() -> void:
 	var path = "res://assets/glitch"
@@ -30,4 +36,13 @@ func load_item_images() -> void:
 			add_file_to_list(fn, path)
 
 func get_random_item_image() -> Dictionary:
-	return _items_images.pick_random()
+	return _item_images.pick_random()
+
+func get_image(index: int) -> Dictionary:
+	return _item_images[index]
+
+func get_random_frame_image() -> CompressedTexture2D:
+	return FRAME_IMAGES.pick_random()
+
+func shuffle_images() -> void:
+	_item_images.shuffle()
