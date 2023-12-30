ecaac8236d573e115169c808ad62678c0921dae4
Renan Valladão <renancvalladao@gmail.com>
2023-12-30 16:19:20 -0300

    Selecting tiles

--- MemoryMadness/scorer/scorer.gd
+++ MemoryMadness/scorer/scorer.gd
@@ -11,10 +11,44 @@ var _pairs_made: int = 0
 
 func _ready():
 	SignalManager.on_tile_selected.connect(on_tile_selected)
+	SignalManager.on_game_exit_pressed.connect(on_game_exit_pressed)
 
 func _process(delta):
 	pass
 
-func on_tile_selected(tile: MemoryTile) -> void:
+func clear_new_game(target_pairs: int) -> void:
+	_selections.clear()
+	_target_pairs = 0
+	_moves_made = 0
+	_target_pairs = target_pairs
+	_tiles = get_tree().get_nodes_in_group(GameManager.GROUP_TILE)
+
+func update_selections() -> void:
+	reveal_timer.start()
+
+func check_pair_made(tile: MemoryTile) -> void:
 	tile.reveal(true)
 	_selections.append(tile)
+	if _selections.size() != 2:
+		return
+	
+	SignalManager.on_selection_disabled.emit()
+	_moves_made += 1
+	
+	update_selections()
+
+func hide_selections() -> void:
+	for s in _selections:
+		s.reveal(false)
+
+func on_tile_selected(tile: MemoryTile) -> void:
+	SoundManager.play_tile_click(sound)
+	check_pair_made(tile)
+
+func _on_reveal_timer_timeout() -> void:
+	hide_selections()
+	_selections.clear()
+	SignalManager.on_selection_enabled.emit()
+
+func on_game_exit_pressed() -> void:
+	reveal_timer.stop()
