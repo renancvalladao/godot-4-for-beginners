af117cefe97dc121f3e1b110fef22c3f4800270b
Renan Valladão <renancvalladao@gmail.com>
2024-01-01 17:38:19 -0300

    Adding player movement

--- FoxyAntics/player/player.gd
+++ FoxyAntics/player/player.gd
@@ -6,6 +6,10 @@ class_name Player
 @onready var animation_player = $AnimationPlayer
 
 const GRAVITY: float = 1000.0
+const RUN_SPEED: float = 120.0
+const MAX_FALL: float = 400.0
+const HURT_TIME: float = 0.3
+const JUMP_VELOCITY: float = -400
 
 func _ready():
 	pass
@@ -14,4 +18,18 @@ func _physics_process(delta):
 	if !is_on_floor():
 		velocity.y += GRAVITY * delta
 	
+	get_input()
+	
 	move_and_slide()
+
+func get_input() -> void:
+	velocity.x = 0
+	
+	if Input.is_action_pressed("left"):
+		velocity.x = -RUN_SPEED
+	elif Input.is_action_pressed("right"):
+		velocity.x = RUN_SPEED
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	velocity.y = clamp(velocity.y, JUMP_VELOCITY, MAX_FALL)
