dae7ef3a61ea6ac6728f3d1a5aa3af54a57478cb
Renan Valladão <renancvalladao@gmail.com>
2024-01-10 13:54:42 -0300

    Adding snail movement

--- FoxyAntics/enemies/enemy_base/enemy_base.gd
+++ FoxyAntics/enemies/enemy_base/enemy_base.gd
@@ -8,8 +8,8 @@ const OFF_SCREEN_KILL_ME: float = 1000.0
 
 @export var default_facing: FACING = FACING.LEFT
 @export var points: int = 1
+@export var speed: float = 30.0
 
-var _speed: float = 30.0
 var _gravity: float = 800.0
 var _facing: FACING = default_facing
 var _player_ref: Player
--- FoxyAntics/enemies/snail/snail.gd
+++ FoxyAntics/enemies/snail/snail.gd
@@ -1,7 +1,29 @@
 extends EnemyBase
 
+@onready var floor_detection = $FloorDetection
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
 func _ready():
 	pass
 
-func _process(delta):
-	pass
+func _physics_process(delta):
+	super._physics_process(delta)
+	
+	if !is_on_floor():
+		velocity.y += _gravity * delta
+	else:
+		velocity.x = speed * _facing
+	
+	move_and_slide()
+	
+	if is_on_floor() && (is_on_wall() || !floor_detection.is_colliding()):
+		flip_me()
+
+func flip_me() -> void:
+	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
+	floor_detection.position.x = floor_detection.position.x * -1
+	
+	if _facing == FACING.LEFT:
+		_facing = FACING.RIGHT
+	else:
+		_facing = FACING.LEFT
